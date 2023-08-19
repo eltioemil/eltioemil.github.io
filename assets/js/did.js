7c44090c8074bcdc21c9b33805b6e8af5d9bd205
Emilio Ureña Ramírez <eltioemil@wAIRdo.local>
2023-08-20 01:27:57 +0200

    Removed file validation

--- assets/js/did.js
+++
@@ -1,6 +0,0 @@
-async function resolveHandle(handle: string) {
-  const res = await fetch(`https://${handle}/.well-known/atproto-did`)
-  const did = await res.text()
-  assert(typeof did === 'string' && did.startsWith('did:'))
-  return did
-}
